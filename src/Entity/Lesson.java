738fffcc2be9d2595c348a037c54ec44fb71efea
Sean Goh <sean28goh@outlook.com>
2020-11-03 14:30:08 +0800

    Update class attributes and methods

--- src/Entity/Lesson.java
+++ src/Entity/Lesson.java
@@ -1,2 +1,70 @@
-package Entity;public class Lesson {
+package Entity;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+public class Lesson {
+    private int indexNo;
+    private String classType;
+    private DayOfWeek day;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private String location;
+    private String weeks;
+
+    public int getIndexNo() {
+        return indexNo;
+    }
+
+    public void setIndexNo(int indexNo) {
+        this.indexNo = indexNo;
+    }
+
+    public String getClassType() {
+        return classType;
+    }
+
+    public void setClassType(String classType) {
+        this.classType = classType;
+    }
+
+    public DayOfWeek getDay() {
+        return day;
+    }
+
+    public void setDay(DayOfWeek day) {
+        this.day = day;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getWeeks() {
+        return weeks;
+    }
+
+    public void setWeeks(String weeks) {
+        this.weeks = weeks;
+    }
 }
