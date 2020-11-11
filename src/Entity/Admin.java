86df0fdaeaccbfed4bf2dc5e48e4e0116dd963b8
MacDboi <epiclol1998@gmail.com>
2020-11-11 12:10:33 +0800

    changed smth

--- src/Entity/Admin.java
+++ src/Entity/Admin.java
@@ -2,6 +2,9 @@ package Entity;
 
 public class Admin {
     private int adminID;
+    private Account account;
     public int getAdminID(){return adminID;}
     public void setAdminID(int adminID){this.adminID = adminID;}
+    public Account getAccount(){return account;}
+    public void setAccount(Account account){this.account = account;}
 }
