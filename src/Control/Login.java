a4e2a353f1fc6f5112d7e53d438a552a4a4afcfa
datmanlyman <jeewern2@gmail.com>
2020-11-18 21:27:21 +0800

    flushed the username

--- src/Control/Login.java
+++ src/Control/Login.java
@@ -42,6 +42,7 @@ public class Login {
         AccountsFromDatabase(domain);                   //missing accessperiod check need read student file
         do {
             System.out.println("Please enter your Username: ");
+            sc.nextLine();
             userName = sc.nextLine();
             System.out.println("Please enter your Password: ");
             password = sc.next();
