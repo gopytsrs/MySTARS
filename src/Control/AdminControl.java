b28a14e3c3504a6791a06c6f3eb90f0e6c044189
MacDboi <epiclol1998@gmail.com>
2020-11-21 17:33:03 +0800

    email for update vacancy

--- src/Control/AdminControl.java
+++ src/Control/AdminControl.java
@@ -508,6 +508,28 @@ public class AdminControl {
                                 } else {
                                     SelectedIndex.get(option2).setVacancy(newVacancy);
                                     System.out.println("Vacancy has been updated");
+                                    if (newVacancy > 0 && !SelectedIndex.get(option2).getWaitList().isEmpty())
+                                    {
+                                        Index indextochange = SelectedIndex.get(option2);
+                                        Student FirstInlist = indextochange.getWaitList().remove(0);
+                                        SelectedIndex.get(option2).assignStudent(FirstInlist);
+                                        if (FirstInlist == null) {
+                                            System.out.println("Error");
+                                            return;
+                                        }
+                                        Student student1 = null;
+                                        for (Student student : studentList) {
+                                            if (student.getEmail().equals(FirstInlist.getEmail())) {
+                                                student1 = student;
+                                            }
+                                        }
+                                        CourseRegistration courseToDrop = new CourseRegistration(indextochange,indextochange.getCourseCode(),indextochange.getCourseName(),C.get(option1).getAu(),student1);
+                                        if (student1 != null) {
+                                            student1.addAssignedCourse(courseToDrop);
+                                            student1.removeWaitList(courseToDrop);
+                                        }
+                                        notificationControl n = new notificationControl(FirstInlist, courseToDrop);
+                                    }
                                 }
                                 break;
                             }
