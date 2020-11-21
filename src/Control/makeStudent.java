03f702ae6abd28e4fef7d360a755cbf0b727732c
datmanlyman <jeewern2@gmail.com>
2020-11-21 21:18:30 +0800

    changed makeStudent

--- src/Control/makeStudent.java
+++ src/Control/makeStudent.java
@@ -15,6 +15,15 @@ public class makeStudent {
         Student student = new Student("Sean Goh", "U1920001A", "sgoh046@e.ntu.edu.sg", 2,"School of Computer Science and Engineering", "M", "Singaporean");
         Student student1 = new Student("James","U1920002K","james046@e.ntu.edu.sg",2,"School of Chemical and Biomedical Engineering","M","Singaporean");
         Student student2 = new Student("Jee Wern","U1920003C","jtchen002@e.ntu.edu.sg",2,"School of Chemical and Biomedical Engineering","M","Singaporean");
+        Student student3 = new Student("Jun Hao", "U1920004D", "jtchen002@e.ntu.edu.sg", 2, "School of Computer Science And Engineering", "M", "Singaporean");
+        Student student4 = new Student("Zhi Ying", "U1920005E", "jtchen002@e.ntu.edu.sg", 2, "Nanyang Business School", "F", "Singaporean");
+        Student student5 = new Student("Wen Cheng", "U1920006F", "jtchen002@e.ntu.edu.sg", 2, "Nanyang Business School", "M", "Singaporean");
+        Student student6 = new Student("Zachary Lam", "U1920007G", "zlam001@e.ntu.edu.sg", 2, "School of Computer Science and Engineering", "M", "Singaporean");
+        Student student7 = new Student("Joseph Phuah", "U1920008H", "jphuah001@e.ntu.edu.sg", 2, "School of Computer Science and Engineering", "M", "Singaporean");
+        Student student8 = new Student("Johnny Hsu", "U1920009I", "jhsu001@e.ntu.edu.sg", 2, "Nanyang Business School", "M", "Taiwanese");
+        Student student9 = new Student("Darren Choo", "U1920010J", "dchoo001@e.ntu.edu.sg", 2, "School of Computer Science and Engineering", "M", "Singaporean");
+        Student student10 = new Student("Lucas Teo", "U1920011K", "lteo001@e.ntu.ed.sg", 2, "School of Chemical and Biomedical Engineering", "M", "Singaporean");
+        Student student11 = new Student("Crystal Chew", "U1920012L", "cchew001@e.ntu.edu.sg", 2, "School of Chemical and Biomedical Engineering", "F", "Singaporean");
 
         S.add(student);
         S.add(student1);
