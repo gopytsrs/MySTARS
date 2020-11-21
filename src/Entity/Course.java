0b073c7f6121fac5e4f5cfa786e87131fdaa34d1
SeanGoh <sean28goh@outlook.com>
2020-11-22 00:55:40 +0800

    Add javadocs for Course

--- src/Entity/Course.java
+++ src/Entity/Course.java
@@ -3,15 +3,42 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Represents a course. A course belongs to one school. A course can have many indexes.
+ */
 public class Course implements Serializable {
+    /**
+     * The course code of the Course.
+     */
     private String courseCode;
+    /**
+     * The course name of the Course.
+     */
     private String courseName;
+    /**
+     * The number of AUs of the Course.
+     */
     private int au;
+    /**
+     * The list of lesson types of the Course.
+     */
     private ArrayList<String> lessonType;
+    /**
+     * The School that offers the Course.
+     */
     private School offeringSchool;      // comment this out if no need
                                         // gerpe/ue attribute
+    /**
+     * The list of Indexes the Course has.
+     */
     private ArrayList<Index> indexList;
 
+    /**
+     * Creates a new Course object based on the given course code, course name and number of AUs.
+     * @param courseCode The course code of the Course.
+     * @param courseName The course name of the Course.
+     * @param au The number of AUs of the Course.
+     */
     public Course(String courseCode, String courseName, int au)
     {
         this.courseCode = courseCode;
@@ -21,38 +48,73 @@ public class Course implements Serializable {
         indexList = new ArrayList<>();
     }
 
+    /**
+     * Gets the course code of the Course.
+     * @return The course code of the Course.
+     */
     public String getCourseCode() {
         return courseCode;
     }
 
+    /**
+     * Sets the course code of the Course.
+     * @param courseCode The course code of the Course.
+     */
     public void setCourseCode(String courseCode) {
         this.courseCode = courseCode;
     }
 
+    /**
+     *  Gets the course name of the Course.
+     * @return The course name of the Course.
+     */
     public String getCourseName() {
         return courseName;
     }
 
+    /**
+     * Sets the course name of the Course.
+     * @param courseName The course name of the Course.
+     */
     public void setCourseName(String courseName) {
         this.courseName = courseName;
     }
 
+    /**
+     * Gets the number of AUs of the Course.
+     * @return The number of AUs of the Course.
+     */
     public int getAu() {
         return au;
     }
 
+    /**
+     * Sets the number of AUs of the Course.
+     * @param au The number of AUs of the Course.
+     */
     public void setAu(int au) {
         this.au = au;
     }
 
+    /**
+     * Gets the list of lesson types of the Course.
+     * @return The list of lesson types of the Course..
+     */
     public ArrayList<String> getLessonType() {
         return lessonType;
     }
 
+    /**
+     * Sets the list of lesson types of the Course.
+     * @param lessonType The list of lesson types of the Course.
+     */
     public void setLessonType(ArrayList<String> lessonType) {
         this.lessonType = lessonType;
     }
 
+    /**
+     * Adds a lesson type to the list of lesson types of the Course.
+     */
     public void addLessonType()
     {
         Scanner sc = new Scanner(System.in);
@@ -61,25 +123,48 @@ public class Course implements Serializable {
         this.lessonType.add(lessonType);
     }
 
+    /**
+     * Adds a lesson type to  the list of lesson types of the Course.
+     * @param lessonType
+     */
     public void addLessonType1(String lessonType)       //use purely for binary
     {
         this.lessonType.add(lessonType);
     }
 
+    /**
+     * Gets the School that offers the Course.
+     * @return The School that offers the Course.
+     */
     public School getOfferingSchool() {
         return offeringSchool;
     }
 
+    /**
+     * Sets the School that offers the Course.
+     * @param offeringSchool The School that offers the Course.
+     */
     public void setOfferingSchool(School offeringSchool) {
         this.offeringSchool = offeringSchool;
     }
 
+    /**
+     * Gets the list of Indexes of the Course.
+     * @return The list of Indexes of the Course.
+     */
     public ArrayList<Index> getIndexList() {
         return indexList;
     }
 
+    /**
+     * Adds an Index to the list of Indexes of the Course.
+     * @param I The Index to add.
+     */
     public void setindexlist(Index I){indexList.add(I);}
 
+    /**
+     * Adds an Index to the list of Indexes of the Course.
+     */
     public void addIndex()
     {
         Scanner sc = new Scanner(System.in);
@@ -118,6 +203,9 @@ public class Course implements Serializable {
         System.out.println("Index Added");
     }
 
+    /**
+     * Prints the list of Indexes of the Course.
+     */
     public void printIndexList(){
         if(this.indexList == null){
             System.out.println("No indexes");
@@ -130,6 +218,10 @@ public class Course implements Serializable {
         }
     }
 
+    /**
+     * Formats the Course object to a String when passed to a print method.
+     * @return
+     */
     @Override
     public String toString() {
         return courseCode +
